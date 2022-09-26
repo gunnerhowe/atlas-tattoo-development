bc4846484dcaaba30d5462440f471123452fe8bd
gunnerhowe <gunnerlevihowe@gmail.com>
2022-09-26 08:37:28 -0500

    Update NavItem.js

--- pages/components/NavItem.js
+++ pages/components/NavItem.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
+
 const NavItem = ({ text, href, active }) => {
   return (
-    <Link href={href}>
+    <Link href={href ?? ''}>
       <a className={'nav__link'}>{text}</a>
     </Link>
   );
