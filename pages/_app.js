6796b3dcc414feae16419449a14b52a6185993fd
gunnerhowe <gunnerlevihowe@gmail.com>
2022-09-25 22:42:51 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 import Head from 'next/head';
 
 import '../styles/globals.css';
-import nav from './nav';
+import Layout from './Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <nav>
+    <Layout>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <Component {...pageProps} />
-    </nav>
+    </Layout>
   );
 }
 
