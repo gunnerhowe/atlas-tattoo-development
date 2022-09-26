2cddf4ee8d478d5d7424cb8b6c56d250d1a6b8bc
gunnerhowe <gunnerlevihowe@gmail.com>
2022-09-25 22:34:44 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
 import '../styles/globals.css';
-import Layout from '../pages/components/layout';
+import Layout from '../layout';
 
 function MyApp({ Component, pageProps }) {
   return (
