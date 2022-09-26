b42f10ad52fa0d4284828a6fc28f86aba0ea799e
gunnerhowe <gunnerlevihowe@gmail.com>
2022-09-25 22:29:11 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,27 @@
 import Head from "next/head";
 import { useState } from "react";
-
+import Link from 'next/link';
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [token, setToken] = useState("");
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  function getDalle2() {
-    setError(false);
-    setLoading(true);
-    fetch(`/api/dalle2?k=${token}&q=${query}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setResults(data.result);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-        setError(true);
-      });
-  }
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create DALLE 2 App</title>
+        <title>Atlas Tattoo Dev</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Create images with <span className={styles.titleColor}>DALLE 2</span></h1>
+        <h1 className={styles.title}><span className={styles.titleColor}>Atlas Tattoo Development</span></h1>
         <p className={styles.description}>
-          <input
-            id="token"
-            type="text"
-            value={token}
-            onChange={(e) => setToken(e.target.value)}
-            placeholder="Bearer Token"
-          />
-          &
-          <input
-            id="query"
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Query"
-          />
-          <button onClick={getDalle2}>Get 6 Images</button>
-        </p>{" "}
-        {error ? (
-          <div className={styles.error}>Something went wrong..Try again</div>
-        ) : (
-          <></>
-        )}
-        {loading && <p>Loading...</p>}
-        <div className={styles.grid}>
-          {results.map((result) => {
-            return (
-              <div className={styles.card}>
-                <img className={styles.imgPreview} src={result.generation.image_path} />
-              </div>
-            );
-          })}
-        </div>
+        </p>
+        <button>
+          <Link href="/generate">Generate</Link>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          </button>
       </main>
     </div>
   );
