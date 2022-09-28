ee6b1f46740911548c482a94b684738f0f51382d
gunnerhowe <gunnerlevihowe@gmail.com>
2022-09-27 21:18:35 -0500

    Update

---
+++ pages/mongo_test.js
@@ -0,0 +1,18 @@
+/* import { connectToDatabase } from "../util/mongodb"
+
+export default function MongoTest({isConnected}) {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+
+export async function getServerSideProps(context) {
+    const { db } = await connectToDatabase()
+
+    return {
+        props: { },
+    }
+} */
