dd26e1efd0c0fe6c35a6039ff42352abdaf71a62
gunnerhowe <gunnerlevihowe@gmail.com>
2022-10-25 10:54:23 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -27,8 +27,8 @@ import FacebookProvider from "next-auth/providers/facebook";
           clientSecret: process.env.FACEBOOK_CLIENT_SECRET
         }),
         TwitterProvider({
-            clientId: process.env.TWITTER_CLIENT_ID,
-            clientSecret: process.env.TWITTER_CLIENT_SECRET
+            clientId: process.env.TWITTER_CONSUMER_ID,
+            clientSecret: process.env.TWITTER_CONSUMER_SECRET
         }),
         /*AppleProvider({
             clientId: process.env.APPLE_ID,
